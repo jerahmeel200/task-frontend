01e17423fea72c1dd2a20233ab81e6840e80f31c
jerahmeel200 <78706437+jerahmeel200@users.noreply.github.com>
2022-09-14 19:37:37 +0100

    change

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://task-backend-app.herokuapp.com/",
 });
 
 const authLink = setContext((_, { headers }) => {
