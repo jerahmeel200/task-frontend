0378da1f2004ec4268bc68563ec9bb0cc95e3471
jerahmeel200 <78706437+jerahmeel200@users.noreply.github.com>
2022-09-14 18:10:40 +0100

    first commit

---
+++ src/ApolloProvider.js
@@ -0,0 +1,32 @@
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { setContext } from "apollo-link-context";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      // authorization: localStorage.getItem("token") || "",
+      authorization: "Bearer " + localStorage.getItem("jwtToken") || "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
